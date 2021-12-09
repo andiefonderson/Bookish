63d31213a439accc315db59a4663f9c90443ff97
beccamason <beccamason28@gmail.com>
2021-12-09 14:42:41 +0000

    Scaffolding added

---
+++ Bookish/Bookish.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Bookish.Web.Areas.Identity.Data;
+using Bookish.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Bookish.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Bookish.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BookishWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BookishWebContextConnection")));
+
+                services.AddDefaultIdentity<BookishWebUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BookishWebContext>();
+            });
+        }
+    }
+}
